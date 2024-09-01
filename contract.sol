66483a3d9c60a14552606706d97e90200062c5a8
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-01 22:57:08 +0530

    first commit

---
+++ contract.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+contract DAO{
+
+    struct Proposal{
+        uint id;
+        string description;
+        uint amount;
+        address recipient;
+        uint votes;
+        uint end;
+        bool isExecuted;
+    }
+
+    mapping(address => bool) public isInvestor;
+    mapping(address => uint) public numOfShares;
+    address[] public investorsList;
+    mapping(address => mapping(uint => bool)) public voted;
+    mapping(uint => Proposal) public proposals;
+    uint public totalShares;
+    uint public totalAmount;
+    uint public contributionTime;
+    address public manager;
+    uint public currProposalId;
+    uint public voteTime;
+    uint public quorum;
+
+    constructor(uint _contributionTime, uint _voteTime, uint _quorum) {
+        require(_quorum<100 && _quorum>0,"Quorum value should be between 0 to 100");
+        contributionTime = block.timestamp + _contributionTime;
+        voteTime = _voteTime;
+        quorum = _quorum;
+        manager = msg.sender;
+    }
+    
+}
